4b51c484605a65c1f4096c781b3fcfacaef3a0df
kaz-taka-1 <k.t.niconico.physics@gmail.com>
2021-07-24 01:32:39 +0900

    リメンバクラスの追加と再生ボタンタップ時の処理の記述

--- app/src/main/java/com/websarva/wings/android/mediasample/MainActivity.kt
+++ app/src/main/java/com/websarva/wings/android/mediasample/MainActivity.kt
@@ -4,6 +4,8 @@ import android.media.MediaPlayer
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     private var _player: MediaPlayer? = null
@@ -13,12 +15,56 @@ class MainActivity : AppCompatActivity() {
 
         _player = MediaPlayer()
         val mediaFileUriStr = "android.resource://${packageName}/${R.raw.mountain_stream}"
-        val mediaFFileUri = Uri.parse(mediaFileUriStr)
+        val mediaFileUri = Uri.parse(mediaFileUriStr)
         _player?.let{
-            it.setDataSource(this@MainActivity,mediaFileUri)
+            it.setDataSource(this@MainActivity, mediaFileUri)
             it.setOnPreparedListener(PlayerPreparedListener())
-            it.setOnCompletionLister(playerCompletionLitener())
+            it.setOnCompletionListener(PlayerCompletionListener())
             it.prepareAsync()
         }
     }
+
+    private inner class PlayerPreparedListener : MediaPlayer.OnPreparedListener{
+        override fun onPrepared(mp:MediaPlayer){
+            val btPlay= findViewById<Button>(R.id.btPlay)
+            btPlay.isEnabled = true
+            val btBack = findViewById<Button>(R.id.btBack)
+            btBack.isEnabled = true
+            val btForward = findViewById<Button>(R.id.btForward)
+            btForward.isEnabled = true
+        }
+    }
+
+    private inner class PlayerCompletionListener : MediaPlayer.OnCompletionListener{
+        override fun onCompletion(mp: MediaPlayer?) {
+            val btPlay = findViewById<Button>(R.id.btPlay)
+            btPlay.setText(R.string.bt_play_play)
+        }
+    }
+
+    fun onPlayButtonClick(view: View){
+        _player?.let{
+            val btPlay = findViewById<Button>(R.id.btPlay)
+            if(it.isPlaying){
+                it.pause()
+                btPlay.setText(R.string.bt_play_play)
+            }
+            else{
+                it.start()
+                btPlay.setText(R.string.bt_play_pause)
+            }
+        }
+    }
+
+    override fun onDestroy(){
+        _player?.let{
+            if(it.isPlaying){
+                it.stop()
+            }
+            it.release()
+        }
+        _player = null
+        super.onDestroy()
+    }
+
 }

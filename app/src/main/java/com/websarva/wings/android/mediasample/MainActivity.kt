9710a77db05c241813c6a611e1284d3ac3ded5b9
kaz-taka-1 <k.t.niconico.physics@gmail.com>
2021-07-24 00:40:25 +0900

    メデイアプレイヤー準備のコードを記述

--- app/src/main/java/com/websarva/wings/android/mediasample/MainActivity.kt
+++ app/src/main/java/com/websarva/wings/android/mediasample/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.websarva.wings.android.mediasample
 
+import android.media.MediaPlayer
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    private var _player: MediaPlayer? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        _player = MediaPlayer()
+        val mediaFileUriStr = "android.resource://${packageName}/${R.raw.mountain_stream}"
+        val mediaFFileUri = Uri.parse(mediaFileUriStr)
+        _player?.let{
+            it.setDataSource(this@MainActivity,mediaFileUri)
+            it.setOnPreparedListener(PlayerPreparedListener())
+            it.setOnCompletionLister(playerCompletionLitener())
+            it.prepareAsync()
+        }
     }
 }

3465d7931f9e692a51f7a7e08c3b3181c63c6bb9
kaz-taka-1 <k.t.niconico.physics@gmail.com>
2021-07-24 15:13:44 +0900

    戻ると進む処理コードを記述

--- app/src/main/java/com/websarva/wings/android/mediasample/MainActivity.kt
+++ app/src/main/java/com/websarva/wings/android/mediasample/MainActivity.kt
@@ -35,6 +35,20 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    fun onBackButtonClick(view: View){
+        _player?.seekTo(0)
+    }
+    fun onForwardButtonClick(view: View){
+        _player?.let{
+            val duration = it.duration
+            it.seekTo(duration)
+            if(!it.isPlaying){
+                it.start()
+            }
+        }
+    }
+
+
     private inner class PlayerCompletionListener : MediaPlayer.OnCompletionListener{
         override fun onCompletion(mp: MediaPlayer?) {
             val btPlay = findViewById<Button>(R.id.btPlay)
